625b58c7d6883b35e000adac12f9c7709b0e81d5
Avraham Bernstein <avraham.bernstein@gmail.com>
2017-08-21 22:20:24 +0300

    v2.1.0
    1. expanded Virtouch descrip
    2. changed build.sh by incorporating metadata.yaml and by implementing a footnote fixup for common footnotes

--- cv/build.sh
+++ cv/build.sh
@@ -2,7 +2,11 @@
 # FILE: build.sh
 # USAGE: build.sh [fast|hh]
 # DESCRIP: build cv html & docx files using pandoc and jinja2
+# PREREQ: bin/j2-cli.py bin/pandoc.sh bin/trim-top.awk
 
+# Do not enable J2 line statements or line comments due to conflict with pandoc ATX (#) headers.
+# Change J2 comments to '{@ ... @}' due to conflict with pandoc '{#internal-link}' notation.
+# j2-cli.py currently is based upon python2 which has poor unicode support which cannot properly process the pandoc template
 
 if [[ -z "$*" ]]; then
 	MODE=""
@@ -10,32 +14,43 @@ else
 	MODE="$1"; # fast|hh
 fi
 
-
 ROOT=cv
 OUT=AvrahamBernstein-CV
-TITLE=AvrahamBernstein:CV
-LANG=en-US
-TS=$(echo '{{DATETIME}}' | j2-cli.py)
+
+TS=$(date --utc +%Y-%m-%dT%H:%M:%SZ)
 
 
 WRITER=html5
 
-# Do not enable J2 line statements or line comments due to conflict with pandoc ATX (#) headers.
-# Change J2 comments to '{@ ... @}' due to conflict with pandoc '{#internal-link}' notation.
+for f in head.html metadata.yaml
+do
+	g=tmp/$f
+	if [[ ! -f $f ]]; then f=$f.j2; fi
+	if [[ ! -f $f ]]; then continue; fi
+	j2-cli.py -Ecomment_start_string='{@' -Ecomment_end_string='@}' -DWRITER=$WRITER -DTS=$TS defs.j2 $f | trim-top.awk > $g
+done
 
-j2-cli.py -Ecomment_start_string='{@' -Ecomment_end_string='@}' -DWRITER=$WRITER -DTS=$TS defs.j2 head.html.j2 | trim-top.awk > tmp/head.html
 j2-cli.py -Ecomment_start_string='{@' -Ecomment_end_string='@}' -DWRITER=$WRITER -DTS=$TS defs.j2 $ROOT.md | trim-top.awk > tmp/$WRITER.md
+pandoc.sh $WRITER --standalone --toc -H tmp/head.html tmp/metadata.yaml tmp/$WRITER.md > tmp/1.html
 
-# Need to add head.html inside template
-
-pandoc.sh $WRITER --standalone --toc --toc-depth=4 -M lang=$LANG -M pagetitle=$TITLE -H tmp/head.html tmp/$WRITER.md > tmp/1.html
-
-# remove footnotes horizontal line
+# remove footnotes: hr + fn[34]
 
 awk '
 /^<section class="footnotes">$/,/^<hr \/>$/ { print "<section class=\"footnotes\">"; next }
+/^<li id="fn[34]"/ { next }
 { print }
 ' tmp/1.html > tmp/2.html
+
+# renumber footnotes: 3,4,5
+
+sed -r \
+-e 's/<a href="#fn3" class="footnoteRef" id="fnref3"><sup>3/<a href="#fn2" class="footnoteRef"><sup>2/' \
+-e 's/<a href="#fn4" class="footnoteRef" id="fnref4"><sup>4/<a href="#fn2" class="footnoteRef"><sup>2/' \
+-e 's/<a href="#fn5" class="footnoteRef" id="fnref5"><sup>5/<a href="#fn3" class="footnoteRef" id="fnref3"><sup>3/' \
+-e 's/<a href="#fnref2">/<a href="#contact-info-links">/' \
+-e 's/<a href="#fnref5">/<a href="#fnref3">/' \
+-e 's/<li id="fn5">/<li id="fn3">/' \
+tmp/2.html > tmp/3.html
 
 # move TOC to the location of marker '%%TOC%%'
 
@@ -59,26 +74,41 @@ state == 1						{
 
 state == 2						{ print; next }
 
-' tmp/2.html > tmp/3.html
+' tmp/3.html > tmp/4.html
 
 # to improve accessibility: h1,h2,h3,h4,h5,h6,li { tabindex: "0"; }
 
-sed -r -e 's/(<)(h[1-6]|li)(>)/\1\2 tabindex="0"\3/g' tmp/3.html > $ROOT.html
+sed -r -e 's/(<)(h[1-6]|li)(>)/\1\2 tabindex="0"\3/g' tmp/4.html > $ROOT.html
 
 if [[ "$MODE" == "fast" ]]; then exit $?; fi
 
-# need to include head.html at the end of the HTML head section
 
-j2-cli.py -Ecomment_start_string='{@' -Ecomment_end_string='@}' -DWRITER=$WRITER -DTS=$TS -DABBREV=eval:True defs.j2 head.html.j2 | trim-top.awk > tmp/head.html
+for f in head.html metadata.yaml
+do
+	g=tmp/$f
+	if [[ ! -f $f ]]; then f=$f.j2; fi
+	if [[ ! -f $f ]]; then continue; fi
+	j2-cli.py -Ecomment_start_string='{@' -Ecomment_end_string='@}' -DWRITER=$WRITER -DTS=$TS -DABBREV=eval:True defs.j2 $f | trim-top.awk > $g
+done
+
 j2-cli.py -Ecomment_start_string='{@' -Ecomment_end_string='@}' -DWRITER=$WRITER -DTS=$TS -DABBREV=eval:True defs.j2 $ROOT.md | trim-top.awk > tmp/$WRITER.md
-pandoc.sh $WRITER --standalone --toc --toc-depth=4 -M lang=$LANG -M pagetitle=$TITLE -H tmp/head.html tmp/$WRITER.md > tmp/1.html
+pandoc.sh $WRITER --standalone --toc -H tmp/head.html tmp/metadata.yaml tmp/$WRITER.md > tmp/1.html
 
-# remove footnotes horizontal line
+# remove footnotes: hr + fn[34]
 
 awk '
 /^<section class="footnotes">$/,/^<hr \/>$/ { print "<section class=\"footnotes\">"; next }
+/^<li id="fn[34]"/ { next }
 { print }
 ' tmp/1.html > tmp/2.html
+
+# renumber footnotes: 3,4 [abbrev version has no 5]
+
+sed -r \
+-e 's/<a href="#fn3" class="footnoteRef" id="fnref3"><sup>3/<a href="#fn2" class="footnoteRef"><sup>2/' \
+-e 's/<a href="#fn4" class="footnoteRef" id="fnref4"><sup>4/<a href="#fn2" class="footnoteRef"><sup>2/' \
+-e 's/<a href="#fnref2">/<a href="#contact-info-links">/' \
+tmp/2.html > tmp/3.html
 
 # move TOC to the location of marker '%%TOC%%'
 
@@ -102,22 +132,38 @@ state == 1						{
 
 state == 2						{ print; next }
 
-' tmp/2.html > tmp/3.html
+' tmp/3.html > tmp/4.html
 
 # to improve accessibility: h1,h2,h3,h4,h5,h6,li { tabindex: "0"; }
 
-sed -r -e 's/(<)(h[1-6]|li)(>)/\1\2 tabindex="0"\3/g' tmp/3.html > ${ROOT}-Abbrev.html
+sed -r -e 's/(<)(h[1-6]|li)(>)/\1\2 tabindex="0"\3/g' tmp/4.html > ${ROOT}-Abbrev.html
 
 
 WRITER=docx
 # TBD: docx supports TOC, but I don't know how to implement it from inside a pandoc template
 # TBD: see options --reference-docx & --reference-odt
 
+for f in metadata.yaml
+do
+	g=tmp/$f
+	if [[ ! -f $f ]]; then f=$f.j2; fi
+	if [[ ! -f $f ]]; then continue; fi
+	j2-cli.py -Ecomment_start_string='{@' -Ecomment_end_string='@}' -DWRITER=$WRITER -DTS=$TS defs.j2 $f | trim-top.awk > $g
+done
+
 j2-cli.py -Ecomment_start_string='{@' -Ecomment_end_string='@}' -DWRITER=$WRITER -DTS=$TS defs.j2 $ROOT.md | trim-top.awk > tmp/$WRITER.md
-pandoc.sh $WRITER --standalone -M lang=$LANG -M pagetitle=$TITLE tmp/$WRITER.md > $ROOT.$WRITER
+pandoc.sh $WRITER --standalone tmp/metadata.yaml tmp/$WRITER.md > $ROOT.$WRITER
+
+for f in metadata.yaml
+do
+	g=tmp/$f
+	if [[ ! -f $f ]]; then f=$f.j2; fi
+	if [[ ! -f $f ]]; then continue; fi
+	j2-cli.py -Ecomment_start_string='{@' -Ecomment_end_string='@}' -DWRITER=$WRITER -DTS=$TS -DABBREV=eval:True defs.j2 $f | trim-top.awk > $g
+done
 
 j2-cli.py -Ecomment_start_string='{@' -Ecomment_end_string='@}' -DWRITER=$WRITER -DTS=$TS -DABBREV=eval:True defs.j2 $ROOT.md | trim-top.awk > tmp/$WRITER.md
-pandoc.sh $WRITER --standalone -M lang=$LANG -M pagetitle=$TITLE tmp/$WRITER.md > ${ROOT}-Abbrev.$WRITER
+pandoc.sh $WRITER --standalone tmp/metadata.yaml tmp/$WRITER.md > ${ROOT}-Abbrev.$WRITER
 
 
 cp -fp ${ROOT}.md xfer/$OUT.md.j2
@@ -137,4 +183,3 @@ cp -fp template.html5 xfer/template.html5
 # if [[ "$MODE" == "hh" ]]; then
 # HEADHUNTER
 # fi
-
