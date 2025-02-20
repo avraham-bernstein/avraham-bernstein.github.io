f71246d69edf67ce1e1ad922159cb95b32fc881b
Avraham Bernstein <avraham.bernstein@gmail.com>
2025-02-20 19:37:22 +0200

    Add files via upload

---
+++ cv/cv-build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -euv
+# vim: fenc=utf-8 ff=unix ft=sh:
+
+# Requirements:
+#   apt: pandoc python3.12 wkhtmltodf 
+#   pip: html5 Markdown markdown-extra pyexpander Pygments 
+
+PREFIX=https://www.AvrahamBernstein.com/cv
+ROOT=AvrahamAbeBernstein-CV
+HTML=$ROOT.html
+MD=$ROOT.md
+PDF=$ROOT.pdf
+
+expander.py --eval "is_pdf=False;html='${PREFIX}/${HTML}';pdf='${PREFIX}/${PDF}'" -f $MD > cv.html.md
+pandoc cv.html.md --metadata-file=cv-metadata.yml -s --columns=96 \
+    -f markdown_phpextra+backtick_code_blocks+header_attributes -t html5 -o $HTML
+
+expander.py --eval  "is_pdf=True;html='${PREFIX}/${HTML}';pdf='${PREFIX}/${PDF}'" -f $MD > cv.pdf.md
+pandoc cv.pdf.md --metadata-file=cv-metadata.yml -s --columns=120 \
+    -f markdown_phpextra+backtick_code_blocks --pdf-engine=wkhtmltopdf -o $PDF
