393ad2c13e5a3caaae6c52923a62f0151e6597d3
Avraham Bernstein <avraham.bernstein@gmail.com>
2026-09-11 13:34:03 +0300

    Add files via upload

--- index.build.sh
+++ index.build.sh
@@ -10,7 +10,12 @@ HTML=$ROOT.html
 MD=$ROOT.md
 TMP_MD=$ROOT.tmp.md
 YML=$ROOT.yml
+PDF=AvrahamAbeBernstein-HomePage.pdf
 
 expander.py -f $MD > $TMP_MD
-pandoc --verbose -f markdown_phpextra+backtick_code_blocks+header_attributes -s --columns=96 \
-	 -t html5 -o $HTML --metadata-file=$YML $TMP_MD
+
+pandoc --verbose -f markdown_phpextra+backtick_code_blocks+header_attributes \
+-s --columns=96 -t html5 -o $HTML --metadata-file=$YML $TMP_MD
+
+pandoc -f markdown_phpextra+backtick_code_blocks+header_attributes --pdf-engine=wkhtmltopdf \
+-s --columns=144 -o $PDF --metadata-file=$YML $TMP_MD
